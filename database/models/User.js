2d37dd15af23cb61c5ab328def548fec5e1a98b4
joao-nilson <joao.nilson@estudante.ufjf.br>
2026-01-19 13:16:11 -0300

    cria mongo bd  e schema de jobs e users

---
+++ database/models/User.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  _id: { type: Number, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  type: {
+    type: String,
+    enum: ['candidate', 'company'],
+    required: true
+  },
+  phone: String,
+  location: String,
+  bio: String,
+  companySize: String,
+  industry: String,
+  website: String,
+  taxId: String,
+  verified: {
+    type: Boolean,
+    default: false
+  },
+  profileCompleted: {
+    type: Boolean,
+    default: false
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return await bcrypt.compare(candidatePassword, this.password);
+};
+
+userSchema.methods.toJSON = function() {
+  const user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+module.exports = mongoose.model('User', userSchema);
